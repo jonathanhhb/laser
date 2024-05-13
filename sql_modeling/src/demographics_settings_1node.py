980e3b06940d8cccf89ed0615c0c5d9fa750bb03
Jonathan Bloedow <jhb4@cornell.edu>
2024-05-13 16:48:13 -0700

    Recommiting some new and changed files that git randomly just blew away on me.

--- sql_modeling/src/demographics_settings_1node.py
+++ sql_modeling/src/demographics_settings_1node.py
@@ -1,17 +1,12 @@
 # Pop & Nodes if building pop from params 
 pop = int(1e7)+1
-#num_nodes = 1
-#num_nodes = 60
-num_nodes = 954
+num_nodes = 1
 nodes = [ x for x in range(num_nodes) ]
 # Epidemiologically Useless Light Agents
 eula_age=5
 
 # Filenames if loading pop from file
-#pop_file="modeled_pop.csv.gz"
-pop_file="engwal_modeled_modified.csv.gz"
-#pop_file="pop_1M_1nodes_seeded.csv.gz"
-eula_file="ew_eula_binned.csv"
-#pop_file_out=f"pop_seeded.csv"
+pop_file="modeled_pop.csv.gz"
+eula_file="eula_binned.csv"
 eula_pop_fits="fits.npy"
 
